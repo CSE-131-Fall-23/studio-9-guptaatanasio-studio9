e14a7591df286e21f981ba12c8113c7e50cbb6f9
atanasioatanasio <atanasioatanasio@atanasios-mbp.wusm-wifi.wucon.wustl.edu>
2023-11-29 15:46:48 -0600

    1

--- src/studio9/Polynomial.java
+++ src/studio9/Polynomial.java
@@ -10,8 +10,7 @@ public class Polynomial {
 	 * Constructs a Polynomial with no terms yet.
 	 */
 	public Polynomial() {
-		//FIXME
-	}
+		list = new LinkedList<>();	}
 
 	
 	/**
@@ -20,7 +19,7 @@ public class Polynomial {
 	 * @return polynomial with added term
 	 */
 	public void addTerm(double coeff) {
-		//FIXME
+		list.add(coeff);
 	}
 	
 	/*
@@ -29,7 +28,16 @@ public class Polynomial {
 	 * Cx^N + Cx^N-1 + ... + Cx + C
 	 */
 	public String toString() {
-		return ""; //FIXME
+		
+		String cache = "";
+		
+		for(int i = 0; i < list.size()-1; i++) {
+			cache += list.get(i) + "x^" + (list.size()-i-1) + " + ";
+		}
+		
+		cache += list.get(list.size()-1);
+		
+		return cache;
 	}
 	
 	/**
@@ -38,12 +46,27 @@ public class Polynomial {
 	 * @return value of polynomial at that x
 	 */
 	public double evaluate(double x) {
-		return 0;//FIXME
+		Double value = 0.0;
+		
+		for(int i = 0; i < list.size(); i++) {
+			
+			value = list.get(i) * Math.pow(x, list.size()-1-i) + value;
+			
+		}
+		
+		return value;
 	}
 
 	
 	public Polynomial derivative() {
-		return null;//FIXME
+		
+		Polynomial derivative = new Polynomial();
+		
+		for(int i = 0; i < list.size()-1; i++) {
+			derivative.addTerm(list.get(i) * (list.size()-1-i));
+		}
+		
+		return derivative;
 	}
 	
 
